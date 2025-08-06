762fd52ef61ab3a4a083be1490ce5c77cd7a7a0b
ujjwalkeshri1201-wq <ujjwalkeshri1201@gmail.com>
2025-08-06 18:55:04 +0530

    Delete xel website final/xel-website last time directory

--- xel website final/xel-website last time/components/footer.tsx
+++
@@ -1,9 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="bg-gray-800 text-white py-12 text-center">
-      <div className="container mx-auto px-4">
-        <p>&copy; 2025 XEL. All rights reserved.</p>
-      </div>
-    </footer>
-  )
-}
